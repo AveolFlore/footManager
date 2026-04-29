71598920966f56786899bf636e1d2ac3f730c2a9
KenzoMind_08 <kenzokng68@gmail.com>
2026-04-29 15:04:57 +0100

    commencement de la partie du controllers

---
+++ views/pages/convocation/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>convocation page</title>
+    <!-- Tailwind CDN -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <?php include_once __DIR__ . '/../../partials/header.php'; ?>
+    <div class="flex">
+        <?php include_once __DIR__ . '/../../partials/sidebar.php'; ?>
+        
+        <main class="flex-1 p-4 md:p-6">
+        <h2>contenue de la convocation</h2>
+        <!-- Ton contenu -->
+    </main>
+</div>
+</body>
+</html>
